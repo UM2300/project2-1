93903b89c1531667957423261dc22272be3feda9
BATUOZOGUL <114209036+BATUOZOGUL@users.noreply.github.com>
2023-10-05 13:05:02 +0200

    commit batu5

--- core/src/com/mygdx/game/TakGameGUI.java
+++ core/src/com/mygdx/game/TakGameGUI.java
@@ -21,220 +21,259 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TakGameGUI extends ApplicationAdapter {
-	private List<TakPiece> pieces;
-	private ArrayList<TakPiece> pieces1;
-	boolean displayOptions = true;
-	boolean isHoveringOverOptions = false;
-	boolean isHoveringOverStart = false;
-	Texture optionsUncoloredTexture;
-	Texture optionsColoredTexture;
+    private TakPiece selectedPiece;
 
-	Texture startColoredTexture;
-	private OrthographicCamera guiCam;
-	private int boardSize = 5; 
-	private float squareSize = 1.5f;
-	Texture startTexture;
-	private static final int VIRTUAL_WIDTH = 1280;
-	private static final int VIRTUAL_HEIGHT = 720;
-	private FitViewport viewport;
+    private List<TakPiece> pieces;
+    private ArrayList<TakPiece> pieces1;
+    boolean displayOptions = true;
+    boolean isHoveringOverOptions = false;
+    boolean isHoveringOverStart = false;
+    Texture optionsUncoloredTexture;
+    Texture optionsColoredTexture;
 
-
-	SpriteBatch batch;
-	Texture img;
-	BitmapFont font;
+    Texture startColoredTexture;
+    private OrthographicCamera guiCam;
+    private int boardSize = 5;
+    private float squareSize = 1.5f;
+    Texture startTexture;
+    private static final int VIRTUAL_WIDTH = 1280;
+    private static final int VIRTUAL_HEIGHT = 720;
+    private FitViewport viewport;
 
 
-	ModelBatch modelBatch;
-	Model boardModel;
-	ModelInstance boardInstance;
-	PerspectiveCamera cam;
-	Environment environment;
-	float imageScale = 0.5f;
+    SpriteBatch batch;
+    Texture img;
+    BitmapFont font;
 
-	boolean displayTitle = true;
 
-	@Override
-	public void create() {
-		pieces = new ArrayList<>();
+    ModelBatch modelBatch;
+    Model boardModel;
+    ModelInstance boardInstance;
+    PerspectiveCamera cam;
+    Environment environment;
+    float imageScale = 0.5f;
 
-		ModelBuilder modelBuilder = new ModelBuilder();
+    boolean displayTitle = true;
 
-		Model stoneModel = modelBuilder.createCylinder(1f, 0.2f, 1f, 20,
-				new Material(ColorAttribute.createDiffuse(Color.GRAY)),
-				Usage.Position | Usage.Normal);
-		for (int i = 0; i < 21; i++) {
-			pieces.add(new TakPiece(TakPiece.Type.STONE, stoneModel));
-		}
+    private Vector3 getScreenCoords(Vector3 worldCoords) {
+        return cam.project(new Vector3(worldCoords));
+    }
 
-		Model capstoneModel = modelBuilder.createCylinder(0.5f, 0.8f, 0.5f, 20,
-				new Material(ColorAttribute.createDiffuse(Color.DARK_GRAY)),
-				Usage.Position | Usage.Normal);
-		pieces.add(new TakPiece(TakPiece.Type.CAPSTONE, capstoneModel));
+    private TakPiece getClickedPiece(float screenX, float screenY) {
+        Vector3 touchPosScreen = new Vector3(screenX, screenY, 0);
+        for (TakPiece piece : pieces) {
+            Vector3 piecePosScreen = getScreenCoords(piece.instance.transform.getTranslation(new Vector3()));
+            float distance = piecePosScreen.dst(touchPosScreen);
+            if (distance < 50) { // 50 is just an example threshold in screen space, adjust accordingly
+                return piece;
+            }
+        }
+        return null;
+    }
 
-		float startingX = boardSize * squareSize + 1;
-		float startingZ = 0;
-		float rowOffset = 1.2f;
-		float nextRowZ = 1.5f;
+    private Vector3 getClickedBoardPosition(float screenX, float screenY) {
+        Vector3 closestPos = null;
+        float minDistance = Float.MAX_VALUE;
 
-		int piecesPerRow = 12;  // Number of pieces in each row.
+        for (int x = 0; x < boardSize; x++) {
+            for (int z = 0; z < boardSize; z++) {
+                Vector3 squareCenterWorld = new Vector3(x * squareSize + squareSize / 2, 0, z * squareSize + squareSize / 2);
+                Vector3 squareCenterScreen = getScreenCoords(squareCenterWorld);
+                float distance = squareCenterScreen.dst(screenX, screenY, 0);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                    closestPos = squareCenterWorld;
 
-		// Position stones
-		for (int i = 0; i < 21; i++) {
-			int x = i % piecesPerRow;
-			int y = i / piecesPerRow;  // Calculate which row the piece is in.
+                }
+            }
+        }
 
-			pieces.get(i).instance.transform.setToTranslation(startingX + x * rowOffset, 0.1f, startingZ + y * nextRowZ);
-		}
+        return (minDistance < 50) ? closestPos : null;  // This checks if the clicked point is within 50 units of a square's center.
+    }
 
-		// Position capstone
-		pieces.get(21).instance.transform.setToTranslation(startingX, 0.25f, startingZ + 2 * nextRowZ);  // Placing the capstone below the stones.
+    @Override
+    public void create() {
+        pieces = new ArrayList<>();
 
-		optionsUncoloredTexture = new Texture(Gdx.files.internal("optionsuncolored.png"));
-		optionsColoredTexture = new Texture(Gdx.files.internal("optionscolored.png"));
-		startColoredTexture = new Texture(Gdx.files.internal("startcolored.png"));
-		startTexture = new Texture(Gdx.files.internal("startuncolored.png"));
+        ModelBuilder modelBuilder = new ModelBuilder();
 
-		batch = new SpriteBatch();
-		guiCam = new OrthographicCamera();
-		viewport = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, guiCam);
-		img = new Texture("badlogic.jpg");
-		font = new BitmapFont();
+        Model stoneModel = modelBuilder.createCylinder(1f, 0.2f, 1f, 20,
+                new Material(ColorAttribute.createDiffuse(Color.GRAY)),
+                Usage.Position | Usage.Normal);
+        for (int i = 0; i < 21; i++) {
+            pieces.add(new TakPiece(TakPiece.Type.STONE, stoneModel));
+        }
 
-		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(15f, 20f, 15f);
-		cam.lookAt(boardSize * squareSize / 2, 0, boardSize * squareSize / 2);
-		cam.near = 1f;
-		cam.far = 500f;
-		cam.update();
+        Model capstoneModel = modelBuilder.createCylinder(0.5f, 0.8f, 0.5f, 20,
+                new Material(ColorAttribute.createDiffuse(Color.DARK_GRAY)),
+                Usage.Position | Usage.Normal);
+        pieces.add(new TakPiece(TakPiece.Type.CAPSTONE, capstoneModel));
 
-		environment = new Environment();
-		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-		modelBatch = new ModelBatch();
-		float startingXLeft = -2;
+        float startingX = boardSize * squareSize + 1;
+        float startingZ = 0;
+        float rowOffset = 1.2f;
+        float nextRowZ = 1.5f;
+
+        int piecesPerRow = 12;  // Number of pieces in each row.
+
+        // Position stones
+        for (int i = 0; i < 21; i++) {
+            int x = i % piecesPerRow;
+            int y = i / piecesPerRow;  // Calculate which row the piece is in.
+
+            pieces.get(i).instance.transform.setToTranslation(startingX + x * rowOffset, 0.1f, startingZ + y * nextRowZ);
+        }
+
+        // Position capstone
+        pieces.get(21).instance.transform.setToTranslation(startingX, 0.25f, startingZ + 2 * nextRowZ);  // Placing the capstone below the stones.
+
+        optionsUncoloredTexture = new Texture(Gdx.files.internal("optionsuncolored.png"));
+        optionsColoredTexture = new Texture(Gdx.files.internal("optionscolored.png"));
+        startColoredTexture = new Texture(Gdx.files.internal("startcolored.png"));
+        startTexture = new Texture(Gdx.files.internal("startuncolored.png"));
+
+        batch = new SpriteBatch();
+        guiCam = new OrthographicCamera();
+        viewport = new FitViewport(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, guiCam);
+        img = new Texture("badlogic.jpg");
+        font = new BitmapFont();
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(15f, 20f, 15f);
+        cam.lookAt(boardSize * squareSize / 2, 0, boardSize * squareSize / 2);
+        cam.near = 1f;
+        cam.far = 500f;
+        cam.update();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        modelBatch = new ModelBatch();
+        float startingXLeft = -2;
 
 // Position stones for the left side
-		for (int i = 0; i < 21; i++) {
-			int x = i % piecesPerRow;
-			int y = i / piecesPerRow;
+        for (int i = 0; i < 21; i++) {
+            int x = i % piecesPerRow;
+            int y = i / piecesPerRow;
 
-			TakPiece piece = new TakPiece(TakPiece.Type.STONE, stoneModel);
-			piece.instance.transform.setToTranslation(startingXLeft - x * rowOffset, 0.1f, startingZ + y * nextRowZ);
-			pieces.add(piece);
-		}
-
-
-		TakPiece capstoneLeft = new TakPiece(TakPiece.Type.CAPSTONE, capstoneModel);
-		capstoneLeft.instance.transform.setToTranslation(startingXLeft, 0.25f, startingZ + 2 * nextRowZ);
-		pieces.add(capstoneLeft);
-
-		// Board creation
-		modelBuilder.begin();
-		Material whiteMat = new Material(ColorAttribute.createDiffuse(Color.WHITE));
-		Material blackMat = new Material(ColorAttribute.createDiffuse(Color.BLACK));
-
-		for (int x = 0; x < boardSize; x++) {
-			for (int z = 0; z < boardSize; z++) {
-				Material mat = (x + z) % 2 == 0 ? whiteMat : blackMat;
-				modelBuilder.part("square_" + x + "_" + z, GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, mat)
-						.box(squareSize * x, 0, squareSize * z, squareSize, 0.15f, squareSize);
-			}
-		}
-		boardModel = modelBuilder.end();
-		boardInstance = new ModelInstance(boardModel);
-	}
+            TakPiece piece = new TakPiece(TakPiece.Type.STONE, stoneModel);
+            piece.instance.transform.setToTranslation(startingXLeft - x * rowOffset, 0.1f, startingZ + y * nextRowZ);
+            pieces.add(piece);
+        }
 
 
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height, true);
-	}
+        TakPiece capstoneLeft = new TakPiece(TakPiece.Type.CAPSTONE, capstoneModel);
+        capstoneLeft.instance.transform.setToTranslation(startingXLeft, 0.25f, startingZ + 2 * nextRowZ);
+        pieces.add(capstoneLeft);
+
+        // Board creation
+        modelBuilder.begin();
+        Material whiteMat = new Material(ColorAttribute.createDiffuse(Color.WHITE));
+        Material blackMat = new Material(ColorAttribute.createDiffuse(Color.BLACK));
+
+        for (int x = 0; x < boardSize; x++) {
+            for (int z = 0; z < boardSize; z++) {
+                Material mat = (x + z) % 2 == 0 ? whiteMat : blackMat;
+                modelBuilder.part("square_" + x + "_" + z, GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, mat)
+                        .box(squareSize * x + squareSize/2, 0.075f, squareSize * z + squareSize/2, squareSize, 0.15f, squareSize);
+
+            }
+        }
+        boardModel = modelBuilder.end();
+        boardInstance = new ModelInstance(boardModel);
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
 
 
 
-	@Override
-	public void render() {
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0.35f, 0.2f, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-		Gdx.gl.glClearColor(0.35f, 0.2f, 0.1f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        guiCam.unproject(touchPos, viewport.getScreenX(), viewport.getScreenY(), viewport.getScreenWidth(), viewport.getScreenHeight());
 
-		Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-		guiCam.unproject(touchPos, viewport.getScreenX(), viewport.getScreenY(), viewport.getScreenWidth(), viewport.getScreenHeight());
+        float imgWidth = startTexture.getWidth() * imageScale;
+        float imgHeight = startTexture.getHeight() * imageScale;
+        float imgX = (VIRTUAL_WIDTH - imgWidth) / 2;
+        float imgY = (VIRTUAL_HEIGHT - imgHeight) / 2;
 
-		float imgWidth = startTexture.getWidth() * imageScale;
-		float imgHeight = startTexture.getHeight() * imageScale;
-		float imgX = (VIRTUAL_WIDTH - imgWidth) / 2;
-		float imgY = (VIRTUAL_HEIGHT - imgHeight) / 2;
+        float optionsImgWidth = optionsUncoloredTexture.getWidth() * imageScale;
+        float optionsImgHeight = optionsUncoloredTexture.getHeight() * imageScale;
+        float optionsImgX = (VIRTUAL_WIDTH - optionsImgWidth) / 2;
+        float optionsImgY = imgY - optionsImgHeight - 10;  // 10 units padding
 
-		float optionsImgWidth = optionsUncoloredTexture.getWidth() * imageScale;
-		float optionsImgHeight = optionsUncoloredTexture.getHeight() * imageScale;
-		float optionsImgX = (VIRTUAL_WIDTH - optionsImgWidth) / 2;
-		float optionsImgY = imgY - optionsImgHeight - 10;  // 10 units padding
+        if (Gdx.input.justTouched()) {
+            if (displayTitle &&
+                    ((touchPos.x >= imgX && touchPos.x <= imgX + imgWidth &&
+                            touchPos.y >= imgY && touchPos.y <= imgY + imgHeight) ||
+                            (touchPos.x >= optionsImgX && touchPos.x <= optionsImgX + optionsImgWidth &&
+                                    touchPos.y >= optionsImgY && touchPos.y <= optionsImgY + optionsImgHeight))) {
+                displayTitle = false;
+            } else if (!displayTitle) {
+                if (selectedPiece == null) {
+                    selectedPiece = getClickedPiece(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+                } else {
+                    Vector3 boardPos = getClickedBoardPosition(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+                    if (boardPos != null) {
+                        selectedPiece.instance.transform.setTranslation(boardPos.x, 0.2f, boardPos.z);
+                        selectedPiece = null;
+                    }
+                }
+            }
+        }
 
+        // Check if mouse is hovering over the start button
+        isHoveringOverStart = touchPos.x >= imgX && touchPos.x <= imgX + imgWidth &&
+                touchPos.y >= imgY && touchPos.y <= imgY + imgHeight;
 
-		if (Gdx.input.justTouched()) {
-			if ((touchPos.x >= imgX && touchPos.x <= imgX + imgWidth &&
-					touchPos.y >= imgY && touchPos.y <= imgY + imgHeight) ||
-					(touchPos.x >= optionsImgX && touchPos.x <= optionsImgX + optionsImgWidth &&
-							touchPos.y >= optionsImgY && touchPos.y <= optionsImgY + optionsImgHeight)) {
-				displayTitle = false;
-			}
-		}
+        // Check if mouse is hovering over the options button
+        isHoveringOverOptions = touchPos.x >= optionsImgX && touchPos.x <= optionsImgX + optionsImgWidth &&
+                touchPos.y >= optionsImgY && touchPos.y <= optionsImgY + optionsImgHeight;
 
-		// Check if mouse is hovering over the start button
-		if (touchPos.x >= imgX && touchPos.x <= imgX + imgWidth &&
-				touchPos.y >= imgY && touchPos.y <= imgY + imgHeight) {
-			isHoveringOverStart = true;
-		} else {
-			isHoveringOverStart = false;
-		}
+        modelBatch.begin(cam);
+        modelBatch.render(boardInstance, environment);
+        for (TakPiece piece : pieces) {
+            modelBatch.render(piece.instance, environment);
+        }
+        modelBatch.end();
 
-		// Check if mouse is hovering over the options button
-		if (touchPos.x >= optionsImgX && touchPos.x <= optionsImgX + optionsImgWidth &&
-				touchPos.y >= optionsImgY && touchPos.y <= optionsImgY + optionsImgHeight) {
-			isHoveringOverOptions = true;
-		} else {
-			isHoveringOverOptions = false;
-		}
+        batch.setProjectionMatrix(guiCam.combined);
+        batch.begin();
 
+        if (displayTitle) {
+            Texture currentStartTexture = isHoveringOverStart ? startColoredTexture : startTexture;
+            batch.draw(currentStartTexture, imgX, imgY, imgWidth, imgHeight);
 
-		modelBatch.begin(cam);
-		modelBatch.render(boardInstance, environment);
-		for (TakPiece piece : pieces) {
-			modelBatch.render(piece.instance, environment);
-		}
-		modelBatch.end();
+            Texture currentOptionsTexture = isHoveringOverOptions ? optionsColoredTexture : optionsUncoloredTexture;
+            batch.draw(currentOptionsTexture, optionsImgX, optionsImgY, optionsImgWidth, optionsImgHeight);
+        }
 
-
-		batch.setProjectionMatrix(guiCam.combined);
-		batch.begin();
-
-		if (displayTitle) {
-			Texture currentStartTexture = isHoveringOverStart ? startColoredTexture : startTexture;
-			batch.draw(currentStartTexture, imgX, imgY, imgWidth, imgHeight);
-
-			Texture currentOptionsTexture = isHoveringOverOptions ? optionsColoredTexture : optionsUncoloredTexture;
-			batch.draw(currentOptionsTexture, optionsImgX, optionsImgY, optionsImgWidth, optionsImgHeight);
-		}
-
-		batch.end();
-	}
-
-
+        batch.end();
+    }
 
 
 
 
-	@Override
-	public void dispose() {
-		batch.dispose();
-		img.dispose();
-		font.dispose();
 
 
-		modelBatch.dispose();
-		boardModel.dispose();
-		startColoredTexture.dispose();
-		optionsUncoloredTexture.dispose();
-		optionsColoredTexture.dispose();
-	}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+        font.dispose();
+
+
+        modelBatch.dispose();
+        boardModel.dispose();
+        startColoredTexture.dispose();
+        optionsUncoloredTexture.dispose();
+        optionsColoredTexture.dispose();
+    }
 }

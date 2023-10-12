85e02f2a894d22491f03db45cb0fcf3c65f5bdfd
AlexaVT03 <an.vanthiel@student.maastrichtuniversity.nl>
2023-10-12 11:48:29 +0200

    swing board

--- core/src/com/mygdx/game/NewGUI.java
+++ core/src/com/mygdx/game/NewGUI.java
@@ -1,6 +1,28 @@
 package com.mygdx.game;
+import java.awt.Color;
+
 import javax.swing.*;
+import javax.swing.JFrame;
 
 public class NewGUI {
+
+
+    
+    
+
+    public static void main(String[] args) {
+
+        
+        JFrame frame = new JFrame("Tak Board Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 800);
+        frame.setResizable(true);
+
+        frame.setVisible(true);
+
+        
+    }
+    
+    
     
 }

bf212eaf810c7d42ad66fd1674584e33af2ebd23
CrabtreeAJ <a.crabtree@student.maastrichtuniversity.nl>
2023-11-15 11:50:34 +0100

    logicBoard

--- core/src/com/mygdx/game/GUI/Baseline_Agent.java
+++ core/src/com/mygdx/game/GUI/Baseline_Agent.java
@@ -8,31 +8,34 @@ import com.mygdx.game.GameLogic.game;
 
 public class Baseline_Agent {
 
-    public board board;
-    public game game;
+    private board logicBoard;
+
+    public Baseline_Agent(board logicBoard){
+        this.logicBoard=logicBoard;
+    }
 
 
     public boolean isAStack (int x, int y) {
-        if (board.board[x][y].size() > 1 ) {
+        if (logicBoard.getBoard()[x][y].size() > 1 ) {
             return true;
         }
         return false;
     }
 
-    public ArrayList<int []> getAvailableTiles (board board, String currentPlayer) {
+    public ArrayList<int []> getAvailableTiles (board logicBoard, String currentPlayer) {
 
         ArrayList<int []> availableTiles = new ArrayList<>();   //ArrayList of arrays 
         
-        for (int i = 0; i < board.getBoard().length; i++) {
-            for (int j = 0; j <  board.getBoard().length; j++) {
+        for (int i = 0; i < logicBoard.getBoard().length; i++) {
+            for (int j = 0; j <  logicBoard.getBoard().length; j++) {
 
-                int tileValue = board.getBoard()[i][j].get(board.getBoard()[i][j].size()-1);  //top piece of the stack
+                int tileValue = logicBoard.getBoard()[i][j].get(logicBoard.getBoard()[i][j].size()-1);  //top piece of the stack
 
                 // if the current player is white and the piece at the tile is 0, 1, 2 or it is empty,
                 // create a copy of the arraylist and add it to availableTiles
                 if (currentPlayer.equals("WHITE")){
 
-                    if((tileValue >= 0 && tileValue <= 2) || board.getBoard()[i][j].isEmpty()) {
+                    if((tileValue >= 0 && tileValue <= 2) || logicBoard.getBoard()[i][j].isEmpty()) {
                         int [] coordinates = {i, j};   
                         availableTiles.add(coordinates);
                     }
@@ -42,7 +45,7 @@ public class Baseline_Agent {
                 // create a copy of the arraylist and add it to availableTiles
                 if (currentPlayer.equals("BROWN")) {
 
-                    if((tileValue >= 3 && tileValue <= 5) || board.getBoard()[i][j].isEmpty()) {
+                    if((tileValue >= 3 && tileValue <= 5) || logicBoard.getBoard()[i][j].isEmpty()) {
                         int[] coordinates = {i, j};    
                         availableTiles.add(coordinates);
                     }

cb47efc748a1b7a79a2b6f7f7ec949f0a706502a
AlinaGavrish <a.gavrish.m@gmail.com>
2024-01-13 12:10:34 +0100

    checked how Python in Java works

---
+++ core/src/com/mygdx/game/Agents/callPython.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.Agents;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class callPython {
+    public static void main(String[] args) throws Exception {
+        //StringBuilder result = new StringBuilder("0");
+        ProcessBuilder processBuilder = new ProcessBuilder("python", "core\\src\\com\\mygdx\\game\\Agents\\python_test.py");
+        processBuilder.redirectErrorStream(true);
+
+        Process process = processBuilder.start();
+                // Read the output of the Python script
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Wait for the process to finish
+        int exitCode = process.waitFor();
+        System.out.println("Python script exited with code: " + exitCode);
+    }
+}

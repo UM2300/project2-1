124e0651a0267dd78705dc75929048baa2398392
AlexaVT03 <an.vanthiel@student.maastrichtuniversity.nl>
2024-01-14 11:16:14 +0100

    calling python methods in java

--- core/src/com/mygdx/game/Agents/python_test.py
+++ core/src/com/mygdx/game/Agents/python_test.py
@@ -1,1 +1,17 @@
-print("Hello!")
+
+# python_test.py
+
+class PythonClass:
+    def greeting(self):
+        print("Hello!")
+
+    def secondMethodTest(self):
+        print("Second method Test")
+
+
+# Create an instance of the class
+python_instance = PythonClass()
+
+# Call the method
+python_instance.greeting()
+python_instance.secondMethodTest()
